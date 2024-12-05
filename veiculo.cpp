cf63c8b0dba40f3dd14a27f79b46651223760684
IsabellyBernardes <113481194+IsabellyBernardes@users.noreply.github.com>
2024-12-05 14:50:50 +0000

    configurando o ambiente

--- veiculo.cpp
+++ veiculo.cpp
@@ -1,5 +1,19 @@
 #include "veiculo.h"
+#include <iostream>
 using namespace std;
 
+void Veiculo::setNumRodas(const int &num_rodas) {
+    this->num_rodas = num_rodas;
+    cout << "Numero de rodas do objeto " << nome << " : " << num_rodas << endl;
+    this->rodas = new Roda[num_rodas];
+
+
+    //this->num_rodas = num_rodas;
+    //cout << "Numero de rodas: " << num_rodas << endl;
+}
+
+int Veiculo::getNumRodas() const {
+    return num_rodas;
+}
 //por algum motivo, aqui não funciona os métodos
 
--- veiculo.h
+++ veiculo.h
@@ -38,19 +38,4 @@ public:
     int getNumRodas() const;
 };
 
-void Veiculo::setNumRodas(const int &num_rodas) {
-    this->num_rodas = num_rodas;
-    cout << "Numero de rodas do objeto " << nome << " : " << num_rodas << endl;
-    this->rodas = new Roda[num_rodas];
-
-
-    //this->num_rodas = num_rodas;
-    //cout << "Numero de rodas: " << num_rodas << endl;
-}
-
-int Veiculo::getNumRodas() const {
-    return num_rodas;
-}
-
-
 #endif //VEICULO_H
